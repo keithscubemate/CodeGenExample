cd42f0d98e0ab78e12ee7940a4397fcc8d623ccd
Austin Jones <austin.jones@uster.com>
2024-04-16 09:17:56 -0400

    first commit

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace ConsoleApp;
+
+partial class Program
+{
+  static void Main(string[] args)
+  {
+    Console.WriteLine("Hey World");
+    HelloFrom("Generated Code");
+  }
+
+  static partial void HelloFrom(string name);
+}
